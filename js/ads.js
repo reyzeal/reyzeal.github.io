6b606b5599c62bcc04716f0260b52e63b83dc5a5
reyzeal <reyzeal@users.noreply.github.com>
2021-01-31 18:06:26 +0000

    deploy: reyzeal/personalBlog@09895e2d82d0c8ef6c568e3764780346b563ed3a

---
+++ js/ads.js
@@ -0,0 +1,6 @@
+(() => {
+    // window.fetch("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js").then(() => console.log("ok")).catch(() => {
+    //     alert("Mohon maaf Ads Blocknya dimatikan terlebih dahulu")
+    //     ___();
+    // })
+})()
