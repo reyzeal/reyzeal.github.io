ab420fd5722c47f45f2cb06e399f255ce7a092aa
reyzeal <reyzeal@users.noreply.github.com>
2021-07-14 11:59:10 +0000

    deploy: reyzeal/personalBlog@ac385f2808e1f628c3cb1a020ca7049a9b4563eb

--- js/ads.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    // window.fetch("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js").then(() => console.log("ok")).catch(() => {
-    //     alert("Mohon maaf Ads Blocknya dimatikan terlebih dahulu")
-    //     ___();
-    // })
-})()
--- js/image.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    let x = $("article img")
-    for(let i of x){
-        if(!$(i).hasClass("img-fluid")){
-            $(i).addClass("img-fluid");
-        }
-    }
-})
